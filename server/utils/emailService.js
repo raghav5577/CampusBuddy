3a645391067c99b3b54c4e46935a8c7be9115487
raghav5577 <raghavmil425@gmail.com>
2026-02-04 20:14:03 +0530

    fix: Switch email transport to use service: gmail for reliability

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,9 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
+    // Use 'service: gmail' which automatically handles host/port/secure settings correctly
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
