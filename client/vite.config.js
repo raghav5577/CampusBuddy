92585fc5d64ccb1a105d2174c8e00ad7af0f3c2f
raghav5577 <raghavmil425@gmail.com>
2026-02-23 20:13:16 +0530

    feat: menu availability toggle, search + fix vite chunk size warning

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,19 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor libs into separate chunks
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'motion': ['framer-motion'],
+          'icons': ['react-icons'],
+          'utils': ['axios', 'socket.io-client', 'react-toastify'],
+        }
+      }
+    }
   }
 })
